df405d06bec51d64dcfa72bcb813add57262a8d3
Hamel Barrero <hamel_-barrero@hotmail.com>
2020-01-24 14:29:00 -0500

    test v1 of shopping cart

--- carts/models.py
+++ carts/models.py
@@ -1,10 +1,14 @@
+import uuid
+
 from django.db import models
 from users.models import User
 from products.models import Product
+from django.db.models.signals import pre_save
 
 
 # Create your models here.
 class Cart(models.Model):
+    cart_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     products = models.ManyToManyField(Product)
     subtotal = models.DecimalField(default=0, max_digits=8, decimal_places=3)
@@ -12,4 +16,12 @@ class Cart(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return ''
+        return self.cart_id
+
+
+def set_cart_id(sender, instance, *args, **kwargs):
+    if not instance.cart_id:
+        instance.cart_id = str(uuid.uuid4())
+
+
+pre_save.connect(set_cart_id, sender=Cart)
--- carts/views.py
+++ carts/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cart
+from products.models import Product
+from .utils import get_or_create_cart
 
 
 # Create your views here.
 def cart(request):
-    user = request.user if request.user.is_authenticated else None
-    cart_id = request.session.get('cart_id')
-    if cart_id:
-        cart = Cart.objects.get(pk=cart_id)
-    else:
-        cart = Cart.objects.create(user=user)
-    request.session['cart_id'] = cart.id    
+    cart = get_or_create_cart(request)
     template_name = 'carts/cart.html'
     return render(request, template_name, {
-
+        'cart': cart
     })
+
+
+def add(request):
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+
+    cart.products.add(product)
+    template_name = 'carts/add.html'
+    return render(request, template_name, {
+        'product': product
+    })
+
+
+def remove(request):
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+
+    cart.products.remove(product)
+
+    return redirect('carts:cart')
