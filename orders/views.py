5b2cb26d1dec9d98d2575c49bccf3bca4e8ad2e5
hamelBarrero <hamel_-barrero@hotmail.com>
2020-01-29 21:44:25 -0500

    Orders of store v2

--- orders/views.py
+++ orders/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def order(request):
-    pass
+    template_name = 'order/order.html'
+    return render(request, template_name, {
+
+    })
